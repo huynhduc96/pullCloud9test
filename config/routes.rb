9559c4e7b3593de1d01eff6fa401a25ffa411964
Ubuntu <ubuntu@ip-172-31-19-81.ec2.internal>
2019-07-17 06:32:14 +0000

    done chapter8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
   get 'static_pages/home'
   get  '/help',    to: 'static_pages#help'
@@ -8,4 +10,7 @@ Rails.application.routes.draw do
   root 'users#index'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
