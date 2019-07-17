d8d4b722ffd6726aeb5820581a3b5889fd69b278
Ubuntu <ubuntu@ip-172-31-19-81.ec2.internal>
2019-07-17 03:08:08 +0000

    done chapter5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
   get 'static_pages/home'
-  get 'static_pages/help'
+  get  '/help',    to: 'static_pages#help'
 
   resources :microposts
   resources :users
   root 'users#index'
+  get  '/signup',  to: 'users#new'
 end
